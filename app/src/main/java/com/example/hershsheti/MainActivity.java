2de4c0b37cf47ff0002cf17851018bdeac9f6205
YogeshPc <developerhappy123@gmail.com>
2020-03-19 18:32:05 +0530

    Add Bottom Navigetion Bar 3 cheking the fragment attachment from MainActivity

--- app/src/main/java/com/example/hershsheti/MainActivity.java
+++ app/src/main/java/com/example/hershsheti/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.hershsheti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.hershsheti.BNB_Fragments.AndajeFragment;
+import com.example.hershsheti.BNB_Fragments.BajarFragment;
+import com.example.hershsheti.BNB_Fragments.HomeFragment;
+import com.example.hershsheti.BNB_Fragments.ParFragment;
+import com.example.hershsheti.BNB_Fragments.SevaDeneGheneFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomView=(BottomNavigationView)findViewById(R.id.bottom_navigation);
+
+        bottomView.setOnNavigationItemSelectedListener(nl);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
     }
+   private BottomNavigationView.OnNavigationItemSelectedListener nl=new BottomNavigationView.OnNavigationItemSelectedListener() {
+       @Override
+       public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+           Fragment selctedFragment=null;
+
+           switch (item.getItemId())
+           {
+               case R.id.bHome:
+                   SelectedFragment(new HomeFragment());
+                   break;
+               case R.id.bBajar:
+                   SelectedFragment(new BajarFragment());
+                   break;
+               case R.id.bSevaDeneGhene:
+                   SelectedFragment(new SevaDeneGheneFragment());
+                   break;
+               case R.id.bAndaje:
+                   SelectedFragment(new AndajeFragment());
+                   break;
+               case R.id.bPar:
+                   SelectedFragment(new ParFragment());
+                   break;
+           }
+
+           return true;
+       }
+   };
+
+
+   private void SelectedFragment(Fragment fragment)
+   {
+       FragmentManager fm=getSupportFragmentManager();
+       FragmentTransaction ft=fm.beginTransaction();
+       ft.replace(R.id.fragment_container,fragment).commit();
+   }
 }
